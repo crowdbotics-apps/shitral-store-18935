5ca435268d51b0667f9c4107d8f925af1dc09d9f
Crowdbotics <team@crowdbotics.com>
2020-07-16 08:35:45 +0000

    Update models

--- backend/shitral_store_18935/wsgi.py
+++ backend/shitral_store_18935/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shitral_store_18935.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shitral_store_18935.settings")
 
 application = get_wsgi_application()
